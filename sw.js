38521726ced5fcf9fb333afa33f76bc2ea8027a1
Waylen <waylen888@gmail.com>
2025-02-19 17:09:32 +0800

    調整 PWA 配置，更新服務工作者註冊邏輯及圖示路徑，改善應用範圍設定

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 // Change this to your repository name
-var GHPATH = '/speedometer';
+var GHPATH = '';
  
 // Choose a different app prefix name
-var APP_PREFIX = 'gppwa_';
+var APP_PREFIX = 'speedometer_';
  
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_00';
+var VERSION = 'version_01';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
